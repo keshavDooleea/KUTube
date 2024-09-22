7e78185bb087e10c27f9df58fae05bfc17aaa119
keshav <kdooleea@yahoo.ca>
2024-09-21 20:52:48 -0400

    feat: added template

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+from utils import contants
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template("layout.html", title=contants.appName)
